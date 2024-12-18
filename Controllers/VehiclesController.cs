3d62eda47195c489968362469c8688ff07040fbf
LukeJonesIS <luke.jones@informed.com>
2024-12-18 14:53:12 +0000

    create vehicle controller

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Software_Foundations_Learning_Programme_.DataStore;
+using Software_Foundations_Learning_Programme_.Models;
+
+namespace Software_Foundations_Learning_Programme_.Controllers
+{
+    [ApiController]
+    [Route("api/Vehicles")]
+    public class VehiclesController : ControllerBase
+    {
+        [HttpGet("{vrn}")] 
+        public ActionResult<VehicleDto> GetVehicles(string vrn)
+        {
+            var VehicleToReturn = VehiclesDataStore.Current.Vehicles
+                .FirstOrDefault(a => a.vrn == vrn);
+
+            if (VehicleToReturn == null){
+                return NotFound();
+            }
+            return Ok(VehicleToReturn);
+        }
+    }
+}
