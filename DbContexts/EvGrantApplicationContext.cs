47adb281e5e3440437efe998e00df9e701a8e0ca
LukeJonesIS <luke.jones@informed.com>
2024-12-19 17:29:07 +0000

    initial seed

--- DbContexts/EvGrantApplicationContext.cs
+++ DbContexts/EvGrantApplicationContext.cs
@@ -9,6 +9,60 @@ namespace Software_Foundations_Learning_Programme_.DbContexts
         public DbSet<Application> Applications {get; set;}
         public DbSet<Vehicle> Vehicles {get; set;}
         public DbSet<Address> Addresses {get; set;}
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>()
+                .HasData(
+                new Address()
+                {
+                    Id = 1,
+                    Address_line1 = "2 Dummy Lane",
+                    Address_line2 = "Fake Town",
+                    City = "London",
+                    Postcode = "FA12_6KE"
+                },
+
+                new Address()
+                {
+                    Id = 2,
+                    Address_line1 = "4 Dummy Lane",
+                    Address_line2 = "Fake Town",
+                    City = "London",
+                    Postcode = "FA12_6KE"
+                },
+
+                new Address()
+                {
+                    Id = 3,
+                    Address_line1 = "3 Fake Street",
+                    Address_line2 = "Dummy Town",
+                    City = "Manchester",
+                    Postcode = "NO7_4RL"
+                }
+                );
+            modelBuilder.Entity<Vehicle>()
+                .HasData(
+                    new Vehicle()
+                {
+                    Vrn = "EVS400",
+                    Make = "Tesla",
+                    Model = "Series S",
+                    Year_made = 2018,
+                    Fuel_type = "electric"
+                },
+
+                new Vehicle()
+                {
+                    Vrn = "CAS300",
+                    Make = "Volkswagon",
+                    Model = "Fox",
+                    Year_made = 2008,
+                    Fuel_type = "petrol"
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
 }
---
+++ Migrations/20241219171122_InitialDataSeed.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Software_Foundations_Learning_Programme_.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "Address_line1", "Address_line2", "City", "Postcode" },
+                values: new object[,]
+                {
+                    { 1, "2 Dummy Lane", "Fake Town", "London", "FA12_6KE" },
+                    { 2, "4 Dummy Lane", "Fake Town", "London", "FA12_6KE" },
+                    { 3, "3 Fake Street", "Dummy Town", "Manchester", "NO7_4RL" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vehicles",
+                columns: new[] { "Vrn", "Fuel_type", "Make", "Model", "Year_made" },
+                values: new object[,]
+                {
+                    { "CAS300", "petrol", "Volkswagon", "Fox", 2008 },
+                    { "EVS400", "electric", "Tesla", "Series S", 2018 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Vehicles",
+                keyColumn: "Vrn",
+                keyValue: "CAS300");
+
+            migrationBuilder.DeleteData(
+                table: "Vehicles",
+                keyColumn: "Vrn",
+                keyValue: "EVS400");
+        }
+    }
+}
