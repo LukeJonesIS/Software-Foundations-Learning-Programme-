892554e4fb8452101a882d14e1e18c441d4cef5d
LukeJonesIS <luke.jones@informed.com>
2024-12-23 14:47:45 +0000

    set up alpha stage ui

--- UI/app/routes.js
+++ UI/app/routes.js
@@ -7,9 +7,10 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
 router.post('/Check-eligible-path/is-vrn-EV', function (req, res) {
-  var parking_answer = req.session.data['confirm-adequate-parking']
-  if (parking_answer != undefined) {
+  var parking_answer = req.session.data['adequate-parking']
+  if (parking_answer == "Yes") {
     res.redirect('/Check-eligible-path/is-vrn-EV')
   } else {
     res.redirect('/Check-eligible-path/Confirmation-pages/not-eligible.html')
