0c4fbe821c28ea3ba6f80f7f5a7bdf4acc7ea678
LukeJonesIS <luke.jones@informed.com>
2025-01-09 12:32:32 +0000

    add validation to input infomation page

--- UI/app/routes.js
+++ UI/app/routes.js
@@ -34,13 +34,3 @@ router.post('/Check-eligible-path/find-eligible-address', function (req, res) {
     res.redirect('/Check-eligible-path/Confirmation-pages/not-eligible.html')
   }
 })
-
-router.post('/Submission-path/check-submission-answers', function (req, res) {
-  var name_answer = req.session.data['user-name']
-  var email_answer = req.session.data['user-email']
-  if (name_answer != '' && email_answer != '') {
-    res.redirect('/Submission-path/check-submission-answers')
-  } else {
-    res.redirect('/Check-eligible-path/Confirmation-pages/not-eligible.html')
-  }
-})
